49fb83f05f2ede548110eacd500d4580b4bf94d9
Dhruvika_Gondaliya <107565032+DhruvikaGondaliya@users.noreply.github.com>
2022-06-28 17:18:19 +0530

    Delete 31.py
    delete file

--- 31.py
+++
@@ -1,7 +0,0 @@
-# Take starting number and ending number from the user and print following series.
-
-sn = int(input("Enter the starting number : "))
-en = int(input("Enter the ending number : "))
-
-for x in range(en, sn, 3):
-  print(x) 
